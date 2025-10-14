25506fd4fba45520754bfbec61df99f0e3db05e0
Fatih Olcay <fatiholcayjava@gmail.com>
2025-10-14 21:13:30 +0300

    Test

---
+++ src/test/java/tests/L01_WebDriverCreate/S01_FirstOtomation.java
@@ -0,0 +1,21 @@
+package tests.L01_WebDriverCreate;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class S01_FirstOtomation {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //The first thing to do is to add a web driver
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.fatiholcay.com");
+
+
+
+        Thread.sleep(3000);
+        driver.close();
+
+
+    }
+}
