0798da33adf7972e1073a1171fea3d4f8bfc07d8
Fatih Olcay <fatiholcayjava@gmail.com>
2025-10-15 16:37:38 +0300

    Test

---
+++ src/test/java/tests/L02_DifferentWebDriver/S01_DifferentWebDriver.java
@@ -0,0 +1,38 @@
+package tests.L02_DifferentWebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class S01_DifferentWebDriver {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://www.fatiholcay.com");
+        driver.manage().window().fullscreen();
+        Thread.sleep(1000);
+
+        driver.manage().window().setPosition(new Point(150,150));
+        Thread.sleep(2000);
+
+        driver.manage().window().setSize(new Dimension(500,500));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+
+
+
+
+
+        Thread.sleep(1000);
+        driver.quit();
+
+    }
+
+}
+
