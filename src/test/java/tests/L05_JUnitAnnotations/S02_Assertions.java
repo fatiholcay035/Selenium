f28989bab1ffa42ec5c4b85ccdb557262268b0d9
Fatih Olcay <fatiholcayjava@gmail.com>
2025-10-18 17:39:58 +0300

    Test

---
+++ src/test/java/tests/L05_JUnitAnnotations/S02_Assertions.java
@@ -0,0 +1,36 @@
+package tests.L05_JUnitAnnotations;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class S02_Assertions {
+
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    String url1 = "https://www.fatiholcay.com";
+    String url2 = "https:/www.fatiholcay.com/";
+
+    @Test
+    public void test01 () {
+        assertEquals(c,a+b);
+    }
+
+    @Test
+    public void test02(){
+        assertEquals(c, a + b);
+    }
+
+    @Test
+    public void test04(){
+        assertNotEquals(b,a+c);
+
+    }
+
+
+
+}
