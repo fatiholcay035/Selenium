386181903a340a258341ea1122f5a90240aef48f
Fatih Olcay <fatiholcayjava@gmail.com>
2025-10-24 22:41:30 +0300

    Test

--- src/test/java/tests/L07_DropdownMenu/S01_DropdownMenu.java
+++ src/test/java/tests/L07_DropdownMenu/S01_DropdownMenu.java
@@ -1,6 +1,10 @@
 package tests.L07_DropdownMenu;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import utilities.ReusableMethods;
 import utilities.TestBase_Each;
 
 public class S01_DropdownMenu extends TestBase_Each {
@@ -9,9 +13,17 @@ public class S01_DropdownMenu extends TestBase_Each {
     @Test
     public void test01(){
 
+        driver.get("https://the-internet.herokuapp.com/dropdown");
 
+        WebElement ddm= driver.findElement(By.id("dropdown"));
+
+        Select select= new Select(ddm);
+
+        select.selectByIndex(1);
+        System.out.println(select.getFirstSelectedOption().getText());
+
+
+        ReusableMethods.wait(2);
     }
-
-
 
 }
