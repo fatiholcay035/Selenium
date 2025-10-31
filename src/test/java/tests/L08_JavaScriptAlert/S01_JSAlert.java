8373a63b8f0f61d5cdffee70d16c6c5d68d78bc3
Fatih Olcay <fatiholcayjava@gmail.com>
2025-10-31 23:57:09 +0300

    Test

---
+++ src/test/java/tests/L08_JavaScriptAlert/S01_JSAlert.java
@@ -0,0 +1,12 @@
+package tests.L08_JavaScriptAlert;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import utilities.ReusableMethods;
+import utilities.TestBase_Each;
+
+public class S01_JSAlert extends TestBase_Each {
+
+
+
+}
